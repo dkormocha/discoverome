0515d28e1fec8a022ddd32d3ddd25cfd571fcef9
dhiraj kormocha <dkormocha@hotmail.com>
2026-05-26 16:13:38 +0100

    correcting config object path

--- app/__init__.py
+++ app/__init__.py
@@ -1,30 +1,19 @@
 import os
-
-from flask import Flask, g, current_app
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from .extensions import db, migrate
 
 def create_app():
-
-    # create and configure the app
     app = Flask(__name__)
-    app.config.from_object('discoverome.default_settings')
+
+    app.config.from_object('app.default_settings')
     app.config.from_envvar('DISCOVEROME_SETTINGS')
 
-    # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-    from extensions import db, migrate
-
     db.init_app(app)
     migrate.init_app(app, db)
 
     return app
-
-#Get db connection
-def get_db():
-    if 'db' not in g:
-        g.db = SQLAlchemy(current_app) 
-    return g.db
